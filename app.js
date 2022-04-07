0d7ec92112b08c87a1adb3a9dcb479b82c1b818e
birdzzz <aaarea123@163.com>
2022-04-08 00:28:01 +0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://birdzzz:T8593existence@cluster0.r38sp.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url  = 'mongodb+srv://birdzzz:T8593existence@cluster0.r38sp.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
